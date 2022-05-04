a8591cd6448c85fd26e70de7accfa642b37f1670
Pierre <pschumacher@tuebingen.mpg.de>
2022-05-04 11:35:33 +0200

    started work

--- tonic/tensorflow/agents/agent.py
+++ tonic/tensorflow/agents/agent.py
@@ -22,8 +22,20 @@ class Agent(agents.Agent):
 
     def save(self, path):
         logger.log(f'\nSaving weights to {path}')
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        logger.log(f'\nSaving weights to {path}')
         self.model.save_weights(path)
+        self.save_buffer(path)
 
     def load(self, path, play=None):
         logger.log(f'\nLoading weights from {path}')
         self.model.load_weights(path)
+        self.load_buffer(path)
+
+    def save_buffer(self, path):
+        self.replay.save(path)
+
+    def load_buffer(self, load_fn, path):
+        self.replay.load(load_fn, path)
+
